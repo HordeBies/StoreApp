f904d7c27ddabaa346cab98a71e124b6a9dff140
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-05-01 10:15:08 +0300

    Add project files.

---
+++ Store.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Store.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
